638b47550d9482d0c66d4249a6c5a12dc716eb29
ArturGorski14 <arturgorski14@gmail.com>
2021-04-15 22:24:50 +0200

    Added tests and some improvements

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 from fastapi import FastAPI
 
+from pydantic import BaseModel
+
 app = FastAPI()
+app.counter = 0
+
+
+class HelloResp(BaseModel):
+    msg: str
 
 
 @app.get("/")
-async def read_main():
-    return {"msg": "Hello World"}
+def root() -> dict:
+    return {"message": "Hello World"}
+
+
+@app.get('/counter')
+def counter() -> str:
+    app.counter += 1
+    return str(app.counter)
+
+
+@app.get("/hello/{name}", response_model=HelloResp)
+def hello_name_view(name: str) -> HelloResp:
+    return HelloResp(msg=f"Hello {name}")
--- test_main.py
+++ test_main.py
@@ -1,5 +1,5 @@
 from fastapi.testclient import TestClient
-
+import pytest
 from main import app
 
 client = TestClient(app)
@@ -8,4 +8,19 @@ client = TestClient(app)
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"msg": "Hello World"}
+    assert response.json() == {"message": "Hello World"}
+
+
+@pytest.mark.parametrize("name", ["Zenek", "Marek", "Alojzy Niezdąży"])
+def test_hello_name(name: str):
+    response = client.get(f"/hello/{name}")
+    assert response.status_code == 200
+    assert response.json()['msg'] == f"Hello {name}"
+
+
+@pytest.mark.parametrize("counter", [2])
+def test_counter(counter):
+    for i in range(counter):
+        response = client.get(f"/counter")
+        assert response.status_code == 200
+        assert response.json() == str(i+1)
