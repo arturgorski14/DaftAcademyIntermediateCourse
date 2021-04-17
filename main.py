b5a7d2d3dc2ea9b1eb8fe0f616480c2f75711e6d
ArturGorski14 <arturgorski14@gmail.com>
2021-04-17 18:07:50 +0200

    Added working api methods (get, delete, put) that returns its request method with tests

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Request
 from pydantic import BaseModel
 
 app = FastAPI()
 app.counter = 0
+
+
+class AppClass:
+    APP_URL="daft-academy-intermediate-2021.heroku.com"
 
 
 class HelloResp(BaseModel):
@@ -11,16 +14,47 @@ class HelloResp(BaseModel):
 
 
 @app.get("/")
-def root() -> dict:
+async def root() -> dict:
     return {"message": "Hello World"}
 
 
 @app.get('/counter')
-def counter() -> str:
+async def counter() -> str:
     app.counter += 1
     return str(app.counter)
 
 
 @app.get("/hello/{name}", response_model=HelloResp)
-def hello_name_view(name: str) -> HelloResp:
+async def hello_name_view(name: str) -> HelloResp:
     return HelloResp(msg=f"Hello {name}")
+
+
+@app.get('/method/')
+async def get_method(request: Request):
+    """Return dict with request method. Success status codes are handled by path decorators."""
+    return {'method': request.method}
+
+
+@app.post('/method/post/')
+async def post_method(item: HelloResp, request: Request):
+    return item
+
+
+@app.delete('/method/delete/{item_id}')
+async def delete_method(item_id: int, request: Request):
+    return {'method': request.method}
+
+
+@app.put('/method/update/{item_id}')
+async def put_method(item_id: int, request: Request):
+    return {'method': request.method}
+
+
+@app.options('/method/options/')
+async def options_method(request: Request):
+    return {'method': request.method}
+
+
+@app.get('/auth/{password}/{password_hash}')
+def auth(password: str='', password_hash: str=''):
+    return pass
--- test_main.py
+++ test_main.py
@@ -24,3 +24,25 @@ def test_counter(counter):
         response = client.get(f"/counter")
         assert response.status_code == 200
         assert response.json() == str(i+1)
+
+
+def test_method():
+    response = client.get(f"/method/")
+    assert response.status_code == 200
+    assert response.json() == {'method': "GET"}
+
+    response = client.post(f"/method/post")
+    assert response.status_code == 307
+    # assert response.json() == {'method': "POST"}
+
+    response = client.delete(f"/method/delete/3")
+    assert response.status_code == 200
+    assert response.json() == {'method': "DELETE"}
+
+    response = client.put(f"/method/update/3")
+    assert response.status_code == 200
+    assert response.json() == {'method': "PUT"}
+
+    response = client.put(f"/method/options")
+    assert response.status_code == 307
+    # assert response.json() == {'options': "OPTIONS"}
