9800225a3f7ebcbb5e6a2bada9a2ddcb7fdce763
ArturGorski14 <arturgorski14@gmail.com>
2021-04-17 18:44:46 +0200

    Redo method(s) and got rid of dummy methods

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Request
 from pydantic import BaseModel
+from hashlib import sha512
 
 app = FastAPI()
 app.counter = 0
@@ -9,52 +10,40 @@ class AppClass:
     APP_URL = "https://daft-academy-intermediate-2021.heroku.com"
 
 
-class HelloResp(BaseModel):
-    msg: str
-
-
 @app.get("/")
 async def root() -> dict:
+    """Return dict with HelloWorld message."""
     return {"message": "Hello World"}
-
-
-@app.get('/counter')
-async def counter() -> str:
-    app.counter += 1
-    return str(app.counter)
-
-
-@app.get("/hello/{name}", response_model=HelloResp)
-async def hello_name_view(name: str) -> HelloResp:
-    return HelloResp(msg=f"Hello {name}")
 
 
 @app.get('/method/')
 async def get_method(request: Request):
-    """Return dict with request method. Success status codes are handled by path decorators."""
+    """Return dict with its request method name."""
     return {'method': request.method}
 
 
-@app.post('/method/post/')
-async def post_method(item: HelloResp, request: Request):
-    return item
-
-
-@app.delete('/method/delete/{item_id}')
-async def delete_method(item_id: int, request: Request):
+@app.post('/method/')
+async def post_method(request: Request):
+    """Return dict with its request method name."""
     return {'method': request.method}
 
 
-@app.put('/method/update/{item_id}')
-async def put_method(item_id: int, request: Request):
+@app.delete('/method/')
+async def delete_method(request: Request):
     return {'method': request.method}
 
 
-@app.options('/method/options/')
+@app.put('/method/')
+async def put_method(request: Request):
+    return {'method': request.method}
+
+
+@app.options('/method/')
 async def options_method(request: Request):
     return {'method': request.method}
 
 
 @app.get('/auth/{password}/{password_hash}')
-def auth(password: str='', password_hash: str=''):
+def auth(password: str, password_hash: str, request: Request):
+    """Check if provided password and password_hash match."""
     pass
--- test_main.py
+++ test_main.py
@@ -5,25 +5,10 @@ from main import app
 client = TestClient(app)
 
 
-def test_read_main():
+def test_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
-
-
-@pytest.mark.parametrize("name", ["Zenek", "Marek", "Alojzy Niezdąży"])
-def test_hello_name(name: str):
-    response = client.get(f"/hello/{name}")
-    assert response.status_code == 200
-    assert response.json()['msg'] == f"Hello {name}"
-
-
-@pytest.mark.parametrize("counter", [2])
-def test_counter(counter):
-    for i in range(counter):
-        response = client.get(f"/counter")
-        assert response.status_code == 200
-        assert response.json() == str(i+1)
 
 
 def test_method():
