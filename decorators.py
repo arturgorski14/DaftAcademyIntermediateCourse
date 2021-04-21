9a987b534efa4ba4bca653e38461be2b8e0f698b
ArturGorski14 <arturgorski14@gmail.com>
2021-04-21 22:23:42 +0200

    Simplified greetings decorator and its test

--- decorators.py
+++ decorators.py
@@ -2,7 +2,6 @@ from functools import wraps  # could be useful to have
 
 
 def greetings(function_to_greet):
-    def inner(*args):
-        val = f'Hello {function_to_greet("".join(args)).title()}'
-        return val
+    def inner(names: str):
+        return f'Hello {function_to_greet(names).title()}'
     return inner
--- tests/test_2_A_jak_art.py
+++ tests/test_2_A_jak_art.py
@@ -3,11 +3,7 @@ from main import get_names_surname
 
 
 @pytest.mark.parametrize('names_surname', [
-    'hello', 'janko janeczko', 'ed edd eddy', '1234'
+    'hello', 'janko janeczko', 'ed edd eddy', '1234', 'anna kowalska', 'janek kos', 'jan sebastian bach'
 ])
 def test_greetings(names_surname: str):
-    ab = f'Hello {names_surname.title()}'
-    print(ab)
-    cd = get_names_surname(names_surname)
-    print(cd)
-    assert ab == cd
+    assert f'Hello {names_surname.title()}' == get_names_surname(names_surname)
