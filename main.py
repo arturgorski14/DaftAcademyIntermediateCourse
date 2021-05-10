70c2434c751c114e8b6c99de69c45524fcd51c62
ArturGorski14 <arturgorski14@gmail.com>
2021-05-10 12:58:10 +0200

    Zadanie 4.1 added Order By in queries

--- main.py
+++ main.py
@@ -151,7 +151,11 @@ async def welcome_token(request: Request, token: str = '', format: str = ''):
 @app.get('/categories', tags=['fourth_lecture'])
 async def categories():
     app.db_connection.row_factory = sqlite3.Row
-    data = app.db_connection.execute('SELECT CategoryID, CategoryName FROM Categories').fetchall()
+    data = app.db_connection.execute('''
+        SELECT CategoryID, CategoryName
+        FROM Categories
+        ORDER BY CategoryID
+    ''').fetchall()
     return {'categories': [{"id": x['CategoryID'], "name": x["CategoryName"]} for x in data]}
 
 
@@ -161,115 +165,10 @@ async def customers():
     data = app.db_connection.execute('''
         SELECT CustomerID, CompanyName, Address, PostalCode, City, Country
         FROM CUSTOMERS
+        ORDER BY CustomerID
     ''').fetchall()
     return {'customers': [{
         "id": x['CustomerID'],
         "name": x["CompanyName"],
         "full_address": f"{x['Address']} {x['PostalCode']} {x['City']} {x['Country']}"
     } for x in data]}
-
-# ----------------------------- DAFTCODE ORIGINAL --------------------------
-# @app.get("/products")
-# async def products():
-#     products = app.db_connection.execute("SELECT ProductName FROM Products").fetchall()
-#     return {
-#         "products": products,
-#         "products_counter": len(products)
-#     }
-#
-#
-# @app.get("/suppliers/{supplier_id}")
-# async def single_supplier(supplier_id: int):
-#     app.db_connection.row_factory = sqlite3.Row
-#     data = app.db_connection.execute(
-#         "SELECT CompanyName, Address FROM Suppliers WHERE SupplierID = :supplier_id",
-#         {'supplier_id': supplier_id}).fetchone()
-#
-#     return data
-#
-#
-# @app.get("/employee_with_region")
-# async def employee_with_region():
-#     app.db_connection.row_factory = sqlite3.Row
-#     data = app.db_connection.execute('''
-#         SELECT Employees.LastName, Employees.FirstName, Territories.TerritoryDescription
-#         FROM Employees JOIN EmployeeTerritories ON Employees.EmployeeID = EmployeeTerritories.EmployeeID
-#         JOIN Territories ON EmployeeTerritories.TerritoryID = Territories.TerritoryID;
-#      ''').fetchall()
-#     return [{"employee": f"{x['FirstName']} {x['LastName']}", "region": x["TerritoryDescription"]} for x in data]
-#
-#
-# # @app.get("/customers")
-# # async def customers():
-# #     app.db_connection.row_factory = lambda cursor, x: x[0]
-# #     artists = app.db_connection.execute("SELECT CompanyName FROM Customers").fetchall()
-# #     return artists
-#
-#
-# class Customer(BaseModel):
-#     company_name: str
-#
-#
-# @app.post("/customers/add")
-# async def customers_add(customer: Customer):
-#     cursor = app.db_connection.execute(
-#         f"INSERT INTO Customers (CompanyName) VALUES ('{customer.company_name}')"
-#     )
-#     app.db_connection.commit()
-#     return {
-#         "CustomerID": cursor.lastrowid,
-#         "CompanyName": customer.company_name
-#     }
-#
-#
-# class Shipper(BaseModel):
-#     company_name: str
-#
-#
-# @app.patch("/shippers/edit/{shipper_id}")
-# async def artists_add(shipper_id: int, shipper: Shipper):
-#     cursor = app.db_connection.execute(
-#         "UPDATE Shippers SET CompanyName = ? WHERE ShipperID = ?", (
-#             shipper.company_name, shipper_id)
-#     )
-#     app.db_connection.commit()
-#
-#     app.db_connection.row_factory = sqlite3.Row
-#     data = app.db_connection.execute(
-#         """SELECT ShipperID AS shipper_id, CompanyName AS company_name
-#          FROM Shippers WHERE ShipperID = ?""",
-#         (shipper_id, )).fetchone()
-#
-#     return data
-#
-#
-# @app.get("/orders")
-# async def orders():
-#     app.db_connection.row_factory = sqlite3.Row
-#     orders = app.db_connection.execute("SELECT * FROM Orders").fetchall()
-#     return {
-#         "orders_counter": len(orders),
-#         "orders": orders,
-#     }
-#
-#
-# @app.delete("/orders/delete/{order_id}")
-# async def order_delete(order_id: int):
-#     cursor = app.db_connection.execute(
-#         "DELETE FROM Orders WHERE OrderID = ?", (order_id, )
-#     )
-#     app.db_connection.commit()
-#     return {"deleted": cursor.rowcount}
-#
-#
-# @app.get("/region_count")
-# async def root():
-#     app.db_connection.row_factory = lambda cursor, x: x[0]
-#     regions = app.db_connection.execute(
-#         "SELECT RegionDescription FROM Regions ORDER BY RegionDescription DESC").fetchall()
-#     count = app.db_connection.execute('SELECT COUNT(*) FROM Regions').fetchone()
-#
-#     return {
-#         "regions": regions,
-#         "regions_counter": count
-#     }
